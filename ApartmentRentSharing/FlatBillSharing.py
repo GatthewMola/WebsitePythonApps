df297294c38ac50f5bf023f95d835fba0e32a4cb
GatthewMola <mattgola@outlook.com>
2023-12-28 15:49:45 -0500

    Working on it.

--- ApartmentRentSharing/FlatBillSharing.py
+++ ApartmentRentSharing/FlatBillSharing.py
@@ -12,7 +12,7 @@ class Bill:
         self.period = period
 
 
-class Flatmates:
+class Roommates:
     """
     Object that contains data about the flatmates, such as
     the name of roomate, number of days in flat in a month,
@@ -23,8 +23,8 @@ class Flatmates:
         self.name = name
         self.days_in_flat = days_in_flat
     
-    def pays(self, bill, flatmate2):
-        weight = (self.days_in_flat / (self.days_in_flat + flatmate2.days_in_flat))
+    def pays(self, bill, roommate2):
+        weight = (self.days_in_flat / (self.days_in_flat + roommate2.days_in_flat))
         to_pay = bill.amount * weight
         return to_pay
 
@@ -38,10 +38,10 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def generate(self, flatmate1, flatmate2, bill):
+    def generate(self, roommate1, roommate2, bill):
 
-        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+        roommate1_pay = str(round(roommate1.pays(bill, roommate2), 2))
+        roommate2_pay = str(round(roommate2.pays(bill, roommate1), 2))
 
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
@@ -51,7 +51,7 @@ class PdfReport:
 
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt="Roommates Bill", border=0, align="C", ln=1)
 
         # Insert Period and Month labels and values
         pdf.set_font(family="Times", size=14, style='B')
@@ -61,12 +61,12 @@ class PdfReport:
 
         # Insert name and due amount for the first flatmate
         pdf.set_font(family="Times", size=12)
-        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
-        pdf.cell(w=175, h=25, txt=flatmate1_pay, border=0, ln=1)
+        pdf.cell(w=100, h=25, txt=roommate1.name, border=0)
+        pdf.cell(w=175, h=25, txt=roommate1_pay, border=0, ln=1)
 
         # Insert name and due amount for the second flatmate
-        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
-        pdf.cell(w=175, h=25, txt=flatmate2_pay, border=0, ln=1)
+        pdf.cell(w=100, h=25, txt=roommate2.name, border=0)
+        pdf.cell(w=175, h=25, txt=roommate2_pay, border=0, ln=1)
 
         pdf.output(self.filename)
         # Need to debug this file path. According to lecture, I need an absolute path for this method to run, syntax is wrong right now
@@ -77,18 +77,18 @@ amount = float(input("Hey User, enter the bill amount: "))
 
 period = input("Enter Period (e.g. December 2020): ")
 
-name1 = input("Name of Flatmate1: ")
-days_in_flat1 = int(input(f"{name1}: Days in Flat: "))
+name1 = input("Name of Roommate1: ")
+days_in_flat1 = int(input(f"{name1}: Days in Apartment: "))
 
-name2 = input("Name of Flatmate2: ")
-days_in_flat2 = int(input(f"{name2}: Days in Flat: "))
+name2 = input("Name of Roommate2: ")
+days_in_flat2 = int(input(f"{name2}: Days in Apartment: "))
 
 current_bill = Bill(amount, period)
-flatmate1 = Flatmates(name1, days_in_flat1)
-flatmate2 = Flatmates(name2, days_in_flat2)
+roommate1 = Roommates(name1, days_in_flat1)
+roommate2 = Roommates(name2, days_in_flat2)
 
-print(f"{flatmate1.name} pays: ", flatmate1.pays(current_bill, flatmate2))
-print(f"{flatmate2.name} pays: ", flatmate2.pays(current_bill, flatmate1))
+print(f"{roommate1.name} pays: ", roommate1.pays(current_bill, roommate2))
+print(f"{roommate2.name} pays: ", roommate2.pays(current_bill, roommate1))
 
 pdf_report = PdfReport(filename=f"{current_bill.period}.pdf")
-pdf_report.generate(flatmate1, flatmate2, current_bill)
+pdf_report.generate(roommate1, roommate2, current_bill)
